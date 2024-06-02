50de4c4b91a3e3a66066776f3e3fb5a3ad29b20e
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-06-02 17:44:12 -0500

    c

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App'; // Ensure this path is correct
+import App from './components/App'; // Ensure this path is correct
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
