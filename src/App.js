d3d61da4a7bddc6430b00f480bfc7a7d74df89e3
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-06-02 20:59:16 -0500

    sycde

--- src/App.js
+++ src/App.js
@@ -6,17 +6,38 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Contact from './components/Contact';
-import Header from './components/Header'; // Import Header component
+import Header from './components/Header';
+import styled from 'styled-components';
 import './App.css';
+
+const NavBar = styled.nav`
+  background-color: #61dafb;
+  padding: 10px;
+  position: fixed;
+  width: 100%;
+  top: 0;
+`;
+
+const NavBarBrand = styled(Link)`
+  color: #000;
+  text-decoration: none;
+  font-size: 1.5em;
+`;
+
+const NavBarLink = styled(Link)`
+  color: #000;
+  text-decoration: none;
+  margin: 0 10px;
+`;
 
 function App() {
   return (
     <Router basename="/reactapptest">
       <div className="App">
-        <Header /> {/* Include Header component */}
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <Header />
+        <NavBar className="navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
-            <Link className="navbar-brand" to="/">Brad Matera</Link>
+            <NavBarBrand className="navbar-brand" to="/">Brad Matera</NavBarBrand>
             <button
               className="navbar-toggler"
               type="button"
@@ -31,24 +52,24 @@ function App() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <Link className="nav-link" to="/about">About</Link>
+                  <NavBarLink className="nav-link" to="/about">About</NavBarLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/projects">Projects</Link>
+                  <NavBarLink className="nav-link" to="/projects">Projects</NavBarLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/skills">Skills</Link>
+                  <NavBarLink className="nav-link" to="/skills">Skills</NavBarLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/experience">Experience</Link>
+                  <NavBarLink className="nav-link" to="/experience">Experience</NavBarLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/contact">Contact</Link>
+                  <NavBarLink className="nav-link" to="/contact">Contact</NavBarLink>
                 </li>
               </ul>
             </div>
           </div>
-        </nav>
+        </NavBar>
 
         <Routes>
           <Route path="/" element={<Home />} />
