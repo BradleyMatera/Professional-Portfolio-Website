69f15ba2c65e76f8b218fbf5884450d7a00a2941
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-06-03 00:15:45 -0500

    Vite

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './App.css';
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const theme = createTheme();
 
