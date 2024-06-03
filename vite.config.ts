cbea43e916b98e391349840e9fd575dd8ea7b691
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-06-03 00:20:58 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build',
-    rollupOptions: {
-      input: 'public/index.html',
-    },
+    outDir: 'dist'
   },
-  publicDir: 'public',
+  base: './',
 });
