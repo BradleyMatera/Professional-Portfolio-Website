2e51a9abe0af55470cec28d6050b4e563003a329
MateraBradley-FS-1 <bfmatera@student.fullsail.edu>
2024-11-02 09:01:04 -0500

    .

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -10,7 +10,7 @@ import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <HeaderNavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
